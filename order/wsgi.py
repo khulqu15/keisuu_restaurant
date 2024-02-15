6a92cf835ec729e3bb7f1fbc724500125f0b0336
Kousuke Takeuchi <ktakeuchi@gmail.com>
2024-02-15 20:49:56 +0900

    add newrelic conf

--- order/wsgi.py
+++ order/wsgi.py
@@ -4,13 +4,19 @@ WSGI config for order project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
 
+import newrelic.agent
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "order.settings")
+
+newrelic.agent.initialize(os.path.join(os.path.dirname(__file__), 'newrelic.ini'))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'order.settings')
 
 application = get_wsgi_application()
+newrelic.agent.wsgi_application()(application)
